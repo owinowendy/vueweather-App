70785b7d7c6fd829d67334274773fd723dab3362
Wendy <wendyowino18@gmail.com>
2023-03-17 16:06:06 +0300

    inor

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" >
+   <main >
+    <img src="./assets/background.pjg" >
+    <div class="search-box">
+      <input type="text" class="search-box" placeholder="Search..">
+      
+    </div>
+   </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data (){
+    return {
+      api_key:'71a20bcec7a998223da537a3f0bbe66b'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+  padding: 0px;
+  
+}
+body{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+
+
+
+</style>
