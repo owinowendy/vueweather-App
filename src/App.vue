ee7f58fe413c9a51c9ecb2b7ecf6c1a039795b13
Wendy <wendyowino18@gmail.com>
2023-05-28 00:38:19 +0300

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 c<template>
-  <div id="app" >
+  <div id="app" :class="typeof weather.main !='undefined' && weather.main.temp >16 ? 'warm':''">
    <main >
     
     <div class="search-box">
@@ -9,14 +9,14 @@ c<template>
       {{ query }}
       
     </div>
-    <div class="weather-wrap">
+    <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
       <div class="location-box">
-        <div class="location">Northhampton,UK </div>
-        <div class="date">Friday 17 March 2023</div>
+        <div class="location">{{weather.name  }},{{ weather.sys.country }}</div>
+        <div class="date">{{ dateBuilder() }}</div>
       </div>
 <div class="weather-box">
-  <div class="temp">9</div>
-  <div class="weather">Rain</div>
+  <div class="temp">{{ Math.round(weather.main.temp) }}c</div>
+  <div class="weather">{{ weather.weather[0].main}}</div>
 </div>
     </div>
    </main>
@@ -24,7 +24,7 @@ c<template>
 </template>
 
 <script>
-
+import axios from 'axios';
 
 export default {
   name: 'App',
@@ -43,15 +43,28 @@ export default {
 fetchweather(e) {
   if (e.key == "Enter")
   {
-    fetch (`${this.api_base}weather? q=${this.query}$units=metric$APPID=${this.api_key}`)
+    fetch (`${this.url_base}weather?q=${this.query}$units=metric$APPID=${this.api_key}`)
     .then(res => {return res.json();
 
     }).then(this.setResults);
   }
+  
 },
 setResults(results){
   this.weather=results;
 }
+  },
+  dateBuilder(){
+    let d= new Date();
+    let months=["January", "February","March","April","May","June","July","August","September","October","November",
+  "December"];
+  let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+
+  let day = days[d.getDate()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+  return `${day} ${date}${month}${year}`;
   }
 
 }
@@ -71,6 +84,9 @@ body{
   background-position: bottom;
   transition: 0.4s;
   
+}
+#app.warm{
+  background-image: url('./assets/warm.jpg.jpg');
 }
 
  main{
